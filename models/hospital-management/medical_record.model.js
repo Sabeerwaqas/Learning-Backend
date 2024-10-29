7859ae041be38736e3b0076f357a77da342d058e
sabeerwaqas050 <sabeerwaqas050@gmail.com>
2024-10-29 20:25:09 +0500

    Chore: Create a new branch and package.json file

--- models/hospital-management/medical_record.model.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const medicalRecordsSchema = new mongoose.Schema({}, { timestamps: true });
-
-export const Records = mongoose.model("Records", medicalRecordsSchema);
