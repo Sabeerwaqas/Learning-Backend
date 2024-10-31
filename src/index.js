49b4a645df48f699df0d85a79925e5517dc85a9f
sabeerwaqas050 <sabeerwaqas050@gmail.com>
2024-10-31 23:34:15 +0500

    Chore: Write code in utils to standardize Api response and error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is running on PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failed!!", error);
+  });
